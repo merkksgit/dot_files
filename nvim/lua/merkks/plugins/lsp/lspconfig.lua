63dc0031c7d5ab36233a6a09b240e7d9263b1ca6
merkksgit <merkks@protonmail.com>
2024-12-13 00:35:22 +0200

    modified:   nvim/lua/merkks/plugins/lsp/lspconfig.lua

--- nvim/lua/merkks/plugins/lsp/lspconfig.lua
+++ nvim/lua/merkks/plugins/lsp/lspconfig.lua
@@ -77,6 +77,16 @@ return {
     --   local hl = "DiagnosticSign" .. type
     --   vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
     -- end
+    vim.diagnostic.config({
+      signs = {
+        text = {
+          [vim.diagnostic.severity.ERROR] = "",
+          [vim.diagnostic.severity.WARN] = "",
+          [vim.diagnostic.severity.HINT] = "󰠠 ",
+          [vim.diagnostic.severity.INFO] = "",
+        },
+      },
+    })
 
     mason_lspconfig.setup_handlers({
       -- default handler for installed servers
