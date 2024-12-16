245fa9d6f070b28963effc28773dbd3579a4e7d0
merkksgit <merkks@protonmail.com>
2024-12-16 18:38:37 +0200

    new file:   nvim/lua/merkks/plugins/noice.lua

---
+++ nvim/lua/merkks/plugins/noice.lua
@@ -0,0 +1,28 @@
+return {
+  {
+    "folke/noice.nvim",
+    event = "VeryLazy",
+    opts = {
+      cmdline = {
+        enabled = true,
+        view = "cmdline_popup",
+        opts = {
+          border = {
+            style = "single",
+          },
+        },
+      },
+      lsp = {
+        override = {
+          ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+          ["vim.lsp.util.stylize_markdown"] = true,
+          ["cmp.entry.get_documentation"] = true,
+        },
+      },
+    },
+    dependencies = {
+      "MunifTanjim/nui.nvim",
+      "rcarriga/nvim-notify",
+    },
+  },
+}
